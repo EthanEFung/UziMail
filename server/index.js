da7e6e9c8af6de9ccc47faa7071c037a7978cbeb
Ethan Fung <EthanEFung@gmail.com>
2017-12-06 16:26:48 -0800

    chore: instantiate server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 3000;
+const app = express();
+const router = require("./routes/index");
+
+app
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use(express.static(path.join(__dirname, "../", "client")))
+  .use(router);
+
+app.listen(port, err => {
+  if (err) throw err;
+  console.log("listening on port ", port);
+});
