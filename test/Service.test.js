e9050987d9d831f9fa7f06aeb021f18e27873e7d
Ethan Fung <EthanEFung@gmail.com>
2017-12-06 18:38:00 -0800

    test: render master and slave provider tests

--- test/Service.test.js
+++ test/Service.test.js
@@ -19,8 +19,22 @@ describe("Service functionality", () => {
     it("should throw an error if no email message is specified");
     it("should throw an error if the user_id is not specified");
     it("should query the database to get Contacts");
+    it("should receive the collection of recipients");
+
     describe("querying the master provider", () => {
-      it("should send ");
+      it("should send a query the provider with recipients and email payloads");
+      it("should receive a response in a timely manner");
+      it("should flag post if unsuccessful");
+      it("should flag post if response is slow");
+      it("should trigger a request to slave provider if flag is raised");
+    });
+
+    describe("querying the slave provider", () => {
+      it("should send a query the provider with recipients and email payloads");
+      it("should receive a response in a timely manner");
+      it("should flag post if unsuccessful");
+      it("should flag post if response is slow");
+      it("should trigger a request to slave provider if flag is raised");
     });
   });
 });
